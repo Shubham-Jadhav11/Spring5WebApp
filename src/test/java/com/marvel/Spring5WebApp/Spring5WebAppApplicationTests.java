3fcb2be84ae1a50812e317028857cbd90847ff22
Shubham-Jadhav11 <jadhav.shubham1805@gmail.com>
2021-12-28 14:16:07 +0530

    Initial commit

---
+++ src/test/java/com/marvel/Spring5WebApp/Spring5WebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marvel.Spring5WebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5WebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
